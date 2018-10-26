8316915d7d188da4730b4ed25aa972266855f993
Jordan Blacker <jblacker@crowdtwist.com>
2018-10-26 15:58:06 -0400

    Fix bug in unregister method (Conversion<?> overload) where either a NPE is thrown, or conversions are not unregistered.

--- src/main/java/com/toddfast/util/convert/TypeConverter.java
+++ src/main/java/com/toddfast/util/convert/TypeConverter.java
@@ -235,7 +235,7 @@ public class TypeConverter {
 		if (conversion!=null) {
 			Object[] keys=conversion.getTypeKeys();
 			synchronized (typeConversions) {
-				if (keys==null) {
+				if (keys!=null) {
 					for (int i=0; i<keys.length; i++) {
 						unregisterTypeConversion(keys[i]);
 					}
